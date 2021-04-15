c861d6eeb914588b9d0c68d6a22f9829ae732db9
Zachary Desira <zacharydesira@kpmg.com.mt>
2021-04-15 08:57:02 +0200

    updated routing

--- src/App.js
+++ src/App.js
@@ -78,13 +78,13 @@ function App() {
     <Router>
       <div className="container">
       {<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />}
-        <Route path='/' exact render={(props) => (
+        <Route path='/tracker-app' exact render={(props) => (
           <>
             {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
           </>
         )} />
-        <Route path='/about' component={About} />
+        <Route path='/tracker-app/about' component={About} />
         <Footer />
       </div>
     </Router>
